3921ac52102092b4bddac62c61ee453f02bc43fe
Pramod Kumar <pramodkumar@Pramods-Laptop.local>
2024-12-16 01:18:49 +0530

    Connected Atlas

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// require('dotenv').config()
+
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from 'express'
+const app = express()
+
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" , (error) => {
+            console.log("coudln't be able to talk to database");
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on PORT ${process.env.PORT}`);
+        })
+        
+    }catch(error){
+        console.error("Error ", error);
+        throw error
+    }
+})()    //iffi function
+
+
+*/
